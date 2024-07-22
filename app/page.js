0f6042b0586aa696d81010c88436300b435c5ad8
kartik5026 <87748398+kartik5026@users.noreply.github.com>
2024-07-22 15:23:29 +0530

    Update page.js
    added head and titles

--- app/page.js
+++ app/page.js
@@ -14,7 +14,9 @@ export default function Home() {
   const Croissant = "https://websitedemos.net/coffee-shop-04/wp-content/uploads/sites/727/2020/11/croissant-01-free-img.png"
   return (
     <>
-      <title>Coffee Shop<title/>
+      <head>
+      <title>Coffee Shop</title>
+    </head>
       {/* <NavBar /> */}
       {/* I used NavBar in layout.js because i want it to visible for all the children of app folder */}
       
